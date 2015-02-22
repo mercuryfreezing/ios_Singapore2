bcfe54ba1b2748a4eaa7d40d1089ee83033846a6
Admin <roshan.prakash@ikompass.com>
2015-02-22 12:12:24 +0800

    first commit

---
+++ iOS_S_2/iOS_S_2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_S_2
+//
+//  Created by Admin on 22/2/15.
+//  Copyright (c) 2015 iKomp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int a = 20;
+        int b = 30;
+        int c = a + b;
+    }
+    return 0;
+}
